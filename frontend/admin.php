732ce5e177c5e1b35d5f4a8bf28f25a5e3ddf8a1
Himansith2004 <as20240514@sci.sjp.ac.lk>
2026-03-06 21:12:08 +0530

    fix the error in select box in district selector

--- frontend/admin.php
+++ frontend/admin.php
@@ -94,9 +94,31 @@ $users = mysqli_query($conn,"SELECT * FROM users ORDER BY created_at DESC");
 
     <div class="filter-section">
       <select id="area">
-        <option value="">Select Area</option>
-        <option value="Kalutara">Kalutara</option>
-        <option value="Colombo">Colombo</option>
+        <option value="Ampara">Ampara</option>
+          <option value="Anuradhapura">Anuradhapura</option>
+          <option value="Badulla">Badulla</option>
+          <option value="Batticaloa">Batticaloa</option>
+          <option value="Colombo">Colombo</option>
+          <option value="Galle">Galle</option>
+          <option value="Gampaha">Gampaha</option>
+          <option value="Hambantota">Hambantota</option>
+          <option value="Jaffna">Jaffna</option>
+          <option value="Kalutara">Kalutara</option>
+          <option value="Kandy">Kandy</option>
+          <option value="Kegalle">Kegalle</option>
+          <option value="Kilinochchi">Kilinochchi</option>
+          <option value="Kurunegala">Kurunegala</option>
+          <option value="Mannar">Mannar</option>
+          <option value="Matale">Matale</option>
+          <option value="Matara">Matara</option>
+          <option value="Monaragala">Monaragala</option>
+          <option value="Mullaitivu">Mullaitivu</option>
+          <option value="Nuwara Eliya">Nuwara Eliya</option>
+          <option value="Polonnaruwa">Polonnaruwa</option>
+          <option value="Puttalam">Puttalam</option>
+          <option value="Ratnapura">Ratnapura</option>
+          <option value="Trincomalee">Trincomalee</option>
+          <option value="Vavuniya">Vavuniya</option>
       </select>
 
       <select id="type">
@@ -114,9 +136,6 @@ $users = mysqli_query($conn,"SELECT * FROM users ORDER BY created_at DESC");
   </div>
 
 
-  <!-- ===============================
-       REGISTERED USERS TABLE
-  ================================= -->
 
   <div class="card">
     <h2>Registered Users</h2>
